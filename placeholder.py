778200e171c8b36cd15d7e81fcfd9e711b859685
Sat Adh <satsang6@gmail.com>
2024-03-23 16:35:15 -0400

    Update placeholder.py

--- placeholder.py
+++ placeholder.py
@@ -35,8 +35,12 @@ minR = round(width/65)
 maxR = round(width/11)
 minDis = round(width/7)
 
+
+
 circles = cv2.HoughCircles(blur, cv2.HOUGH_GRADIENT, 1, minDis, param1=100, param2=25, minRadius=minR, maxRadius=maxR)
 
+if circles is None:
+	circles = [[[0]]]
 
 # perform edge detection, then perform a dilation + erosion to
 # close gaps in between object edges
@@ -63,12 +67,13 @@ for c in cnts:
 
 	# compute the rotated bounding box of the contour
 	orig = image.copy()
+
+
 	box = cv2.minAreaRect(c)
 	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
 	box = np.array(box, dtype="int")
 
-	print(box)
-
+	
 	# order the points in the contour such that they appear
 	# in top-left, top-right, bottom-right, and bottom-left
 	# order, then draw the outline of the rotated bounding
@@ -93,50 +98,75 @@ for c in cnts:
 	(tlblX, tlblY) = midpoint(tl, bl)
 	(trbrX, trbrY) = midpoint(tr, br)
 
-	# draw the midpoints on the image
-	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+	(tX, tY) = midpoint(tl, br)
+	cv2.circle(orig, (int(tX), int(tY)), 5, (0, 0, 0), -1)
 
-	# draw lines between the midpoints
-	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-		(255, 0, 255), 2)
-	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-		(255, 0, 255), 2)
+	circleFound = False
 
-	cv2.circle(orig, (midpoint((int(tltrX), int(tltrY)),(int(blbrX), int(blbrY)))), 5, (0,0,0), -1)
+	if circles is not None:
+		for c in circles[0]:
+			print("ciclrs[0]: ", circles[0])
+			if abs(c[0] - tX) <= 6 and abs(c[1] - tY) <= 6:
+				circleFound = True
+				circles = np.round(circles[0, :]).astype("int")
+				
+				for (x, y, r) in circles:
+					cv2.circle(output, (x, y), r, (0, 255, 0), 2)
+					cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+				cv2.imshow("result", np.hstack([image, output]))
+				cv2.waitKey()
+				
+			
+				break
 
-	# compute the Euclidean distance between the midpoints
-	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+		if circleFound == False:
+			cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+			cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+			cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+			cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
 
-	# if the pixels per metric has not been initialized, then
-	# compute it as the ratio of pixels to supplied metric
-	# (in this case, inches)
-	if pixelsPerMetric is None:
-		pixelsPerMetric = dB / args["width"]
+			# draw lines between the midpoints
+			cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+				(255, 0, 255), 2)
+			cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+				(255, 0, 255), 2)
 
-	# compute the size of the object
-	dimA = dA / pixelsPerMetric
-	dimB = dB / pixelsPerMetric
 
-	# draw the object sizes on the image
-	cv2.putText(orig, "{:.1f}in".format(dimA),
-		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
-	cv2.putText(orig, "{:.1f}in".format(dimB),
-		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (255, 255, 255), 2)
+						
 
-	# # show the output image
-	cv2.imshow("Image", orig)
-	cv2.waitKey(0)
+			# compute the Euclidean distance between the midpoints
+			dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+			dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-if circles is not None:
-		circles = np.round(circles[0, :]).astype("int")
-		for (x, y, r) in circles:
-			cv2.circle(output, (x, y), r, (0, 255, 0), 2)
-			cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-cv2.imshow("result", np.hstack([image, output]))
-cv2.waitKey()
+			# if the pixels per metric has not been initialized, then
+			# compute it as the ratio of pixels to supplied metric
+			# (in this case, inches)
+			if pixelsPerMetric is None:
+				pixelsPerMetric = dB / args["width"]
+
+			# compute the size of the object
+			dimA = dA / pixelsPerMetric
+			dimB = dB / pixelsPerMetric
+
+			# draw the object sizes on the image
+			cv2.putText(orig, "{:.1f}in".format(dimA),
+				(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+				0.65, (255, 255, 255), 2)
+			cv2.putText(orig, "{:.1f}in".format(dimB),
+				(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+				0.65, (255, 255, 255), 2)
+
+			# # show the output image
+			cv2.imshow("Image", orig)
+			cv2.waitKey(0)
+
+
+
+# if circles is not None:
+# 	circles = np.round(circles[0, :]).astype("int")
+# 	print(circles)
+# 	for (x, y, r) in circles:
+# 		cv2.circle(output, (x, y), r, (0, 255, 0), 2)
+# 		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+# cv2.imshow("result", np.hstack([image, output]))
+# cv2.waitKey()
